c57786e284c5a0f19062facbdd044e7478bf4c45
Augusto-221208 <a.burigolocks@gmail.com>
2026-07-08 18:50:12 -0300

    Add files via upload

--- serve.js
+++ serve.js
@@ -1,256 +1,92 @@
-const express = require("express");
-const mysql = require("mysql2");
-const cors = require("cors");
+import Fastify from "fastify";
+import mysql from "mysql2/promise";
+import cors from "@fastify/cors";
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const db = mysql.createConnection({
+const sql = await mysql.createPool({
     host: "localhost",
     user: "root",
     password: "",
     database: "loja_games"
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log("Erro ao conectar ao banco.");
-        return;
+const servidor = Fastify();
+
+await servidor.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+});
+servidor.get("/categorias", async () => {
+
+    const [resultado] = await sql.query(
+        "SELECT * FROM categorias"
+    );
+
+    return resultado;
+
+});
+servidor.get("/categorias/:id", async (request, reply) => {
+
+    const id = request.params.id;
+
+    const [resultado] = await sql.query(
+        "SELECT * FROM categorias WHERE id=?",
+        [id]
+    );
+
+    return resultado;
+
+});
+servidor.post("/categorias", async (request, reply) => {
+
+    const { nome } = request.body;
+
+    if (!nome) {
+        return reply.status(400).send({
+            erro: "Nome obrigatório!"
+        });
     }
 
-    console.log("Banco conectado!");
-});
-
-
-app.get("/categorias", (req, res) => {
-
-    db.query("SELECT * FROM categorias", (err, resultado) => {
-
-        if (err) return res.status(500).json(err);
-
-        res.json(resultado);
-
-    });
-
-});
-
-app.get("/categorias/:id", (req, res) => {
-
-    db.query(
-        "SELECT * FROM categorias WHERE id=?",
-        [req.params.id],
-        (err, resultado) => {
-
-            if (err) return res.status(500).json(err);
-
-            res.json(resultado);
-
-        }
-    );
-
-});
-
-app.post("/categorias", (req, res) => {
-
-    const { nome } = req.body;
-
-    db.query(
+    await sql.query(
         "INSERT INTO categorias(nome) VALUES(?)",
-        [nome],
-        (err, resultado) => {
-
-            if (err) return res.status(500).json(err);
-
-            res.json({
-                mensagem: "Categoria cadastrada!",
-                resultado
-            });
-
-        }
+        [nome]
     );
 
-});
-
-app.put("/categorias/:id", (req, res) => {
-
-    const { nome } = req.body;
-
-    db.query(
-        "UPDATE categorias SET nome=? WHERE id=?",
-        [nome, req.params.id],
-        (err, resultado) => {
-
-            if (err) return res.status(500).json(err);
-
-            res.json({
-                mensagem: "Categoria atualizada!",
-                resultado
-            });
-
-        }
-    );
-
-});
-
-app.delete("/categorias/:id", (req, res) => {
-
-    db.query(
-        "DELETE FROM categorias WHERE id=?",
-        [req.params.id],
-        (err, resultado) => {
-
-            if (err) return res.status(500).json(err);
-
-            res.json({
-                mensagem: "Categoria removida!",
-                resultado
-            });
-
-        }
-    );
-
-});
-
-app.get("/produtos", (req, res) => {
-
-    const sql = `
-    SELECT
-        produtos.id,
-        produtos.nome,
-        produtos.descricao,
-        produtos.preco,
-        produtos.imagem,
-        categorias.nome AS categoria
-    FROM produtos
-    INNER JOIN categorias
-        ON produtos.categoria_id = categorias.id
-    `;
-
-    db.query(sql, (err, resultado) => {
-
-        if (err) return res.status(500).json(err);
-
-        res.json(resultado);
-
+    reply.status(201).send({
+        mensagem: "Categoria cadastrada!"
     });
 
 });
+servidor.put("/categorias/:id", async (request, reply) => {
 
-app.get("/produtos/:id", (req, res) => {
+    const id = request.params.id;
+    const { nome } = request.body;
 
-    db.query(
-        "SELECT * FROM produtos WHERE id=?",
-        [req.params.id],
-        (err, resultado) => {
-
-            if (err) return res.status(500).json(err);
-
-            res.json(resultado);
-
-        }
+    await sql.query(
+        "UPDATE categorias SET nome=? WHERE id=?",
+        [nome, id]
     );
 
+    reply.send({
+        mensagem: "Categoria atualizada!"
+    });
+
 });
+servidor.delete("/categorias/:id", async (request, reply) => {
 
-app.post("/produtos", (req, res) => {
+    const id = request.params.id;
 
-    const {
-        nome,
-        descricao,
-        preco,
-        imagem,
-        categoria_id
-    } = req.body;
-
-    db.query(
-        `INSERT INTO produtos
-        (nome, descricao, preco, imagem, categoria_id)
-        VALUES (?, ?, ?, ?, ?)`,
-        [
-            nome,
-            descricao,
-            preco,
-            imagem,
-            categoria_id
-        ],
-        (err, resultado) => {
-
-            if (err) return res.status(500).json(err);
-
-            res.json({
-                mensagem: "Produto cadastrado!",
-                resultado
-            });
-
-        }
+    await sql.query(
+        "DELETE FROM categorias WHERE id=?",
+        [id]
     );
 
-});
-
-app.put("/produtos/:id", (req, res) => {
-
-    const {
-        nome,
-        descricao,
-        preco,
-        imagem,
-        categoria_id
-    } = req.body;
-
-    db.query(
-        `UPDATE produtos
-        SET
-            nome=?,
-            descricao=?,
-            preco=?,
-            imagem=?,
-            categoria_id=?
-        WHERE id=?`,
-        [
-            nome,
-            descricao,
-            preco,
-            imagem,
-            categoria_id,
-            req.params.id
-        ],
-        (err, resultado) => {
-
-            if (err) return res.status(500).json(err);
-
-            res.json({
-                mensagem: "Produto atualizado!",
-                resultado
-            });
-
-        }
-    );
+    reply.send({
+        mensagem: "Categoria removida!"
+    });
 
 });
-
-app.delete("/produtos/:id", (req, res) => {
-
-    db.query(
-        "DELETE FROM produtos WHERE id=?",
-        [req.params.id],
-        (err, resultado) => {
-
-            if (err) return res.status(500).json(err);
-
-            res.json({
-                mensagem: "Produto removido!",
-                resultado
-            });
-
-        }
-    );
-
-});
-
-app.listen(3000, () => {
-
-    console.log("Servidor rodando em http://localhost:3000");
-
+servidor.listen({
+    port: 3000
+}, () => {
+    console.log("Servidor rodando!");
 });
